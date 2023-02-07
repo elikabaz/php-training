499d33c9cc8f24f8765b7e8cbfa6072065075e79
elahe <elahe.khabaz.bavani@gmail.com>
2023-02-07 21:44:58 +0330

    second

---
+++ home1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>second training</title>
+</head>
+<body>
+    <?php 
+    # I want 
+    $one=25;
+    $two=30.5;
+    $plus=$one+$two;
+    $plus/=2;
+    echo $plus;
+    ?>
+</body>
+</html>
