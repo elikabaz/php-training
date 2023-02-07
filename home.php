1754f75fe9fe1acf754de251a8596f935a772056
elahe <elahe.khabaz.bavani@gmail.com>
2023-02-07 20:33:35 +0330

    first

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>first training</title>
+</head>
+<body>
+    <?php 
+    //I want print hello world
+        echo'hello world'?> 
+</body>
+</html>
