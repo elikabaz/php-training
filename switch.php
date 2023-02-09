66df4427d8d79d401842fc85712f804f6f4af9a9
elahe <elahe.khabaz.bavani@gmail.com>
2023-02-09 21:43:05 +0330

    fourth

---
+++ switch.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $n=5;
+    switch($n)
+    {
+    case 1:
+        echo"Shanbe";
+        break;
+    case 2:
+        echo"Yek Shanbe";
+        break;
+    case 3:
+        echo"Do Shanbe";
+        break;
+    case 4:
+        echo"Se Shanbe";
+        break;
+    case 5:
+        echo"ChaharShanbe";
+        break;
+    case 6:
+        echo"Panj Shanbe";
+        break;
+    case 7:
+        echo"Jome";
+        break;
+    }
+    ?>
+
+</body>
+</html>
