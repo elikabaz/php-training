7b76b26ffbba660be01414cca87c43ff7099dc69
elahe <elahe.khabaz.bavani@gmail.com>
2023-02-10 20:39:56 +0330

    for&foreach

---
+++ for&foreach.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        $list=['ali',24,'student','onlychild',4];
+        $i=0;
+        for($i=4;$i>=0;$i--){
+            echo $list[$i] ."<br>";
+        }
+    ?>
+    <p>-------------------------------------------------------------------------------------------------------</p>
+    <?php
+        $list=['ali',24,'student','onlychild',4];
+        $i=0;
+        for($i=0;$i<=4;$i++){
+            echo $list[$i] ."<br>";
+        }
+    ?>
+
+<p>-------------------------------------------------------------------------------------------------------</p>
+ <?php
+        $list=['ali',24,'student','onlychild',4];
+        
+        foreach($list as $item){
+
+            echo $item ."<br>";
+        }
+    ?>
+<p>-------------------------------------------------------------------------------------------------------</p>
+<?php
+        $list=['ali',24,'student','onlychild',4];
+        
+        foreach($list as $key=>$item-1){
+
+            echo $key.' :::> '.$item ."<br>";
+        }
+    ?>
+
+</body>
+</html>
