6610e19ce4eca10176e368d4fae383731ad88681
elahe <elahe.khabaz.bavani@gmail.com>
2023-02-22 18:24:45 +0330

    upload file to db

--- uploadfile/index.php
+++ uploadfile/index.php
@@ -1,6 +1,8 @@
 <?php
+require_once('../../helpers/db.php');////import db
 
 if(isset($_FILES["image"])){
+    // die(json_encode($_FILES['image'] ,JSON_PRETTY_PRINT));
 
     if(file_exists($_FILES['image']['tmp_name'])){
 
@@ -12,18 +14,25 @@ if(isset($_FILES["image"])){
 
             mkdir('uploads');
         }
-       
-
              if(move_uploaded_file($_FILES['image']['tmp_name'] , $path)){
             /////just work at $_POST/////move files to destination address
 
                 echo $path;
+
+                $query = $conn->prepare("INSERT INTO pictures (name, created_at) VALUES (?, ?)");////be ready to execute query
+
+                $query->bind_param("ss", $_FILES['image']['name'] , date("Y/m/d"));////bind sql query
+
+                $result=$query->execute();
+
+                echo $result ? 'success' :'error';
+
              }else{
-                  echo 'error';
+                  echo 'upload error';
                 }
         
         }else{
-            echo 'error';
+            echo 'file not found';
         }
 
 }else{
--- uploadfile/upload.php
+++ uploadfile/upload.php
@@ -1,16 +1,15 @@
 
 <?php
-
+require_once('../../helpers/db.php');
 ?>
-
-
 
 <html>
 <head>
 </head>
 <body>
-    <form action="/index.php" method="post" enctype="multipart/form-data">
+    <form action="./index.php" method="post" enctype="multipart/form-data">
         <input type="file" name="image">
+
         <button type="submit">upload</button>
     </form>
 </body>
