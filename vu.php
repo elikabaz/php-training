a96a36b0f5f35b3ec9bdc42c9d8ce084b19b6d80
elahe <elahe.khabaz.bavani@gmail.com>
2023-02-22 10:57:43 +0330

    g

---
+++ vu.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <form action="/index.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="image">
+            <button type="submit">upload</button>
+        </form>
+    </body>
+</html>
