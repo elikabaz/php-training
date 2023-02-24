fd086063f2e5024d220a0f09181b13a4089b5290
elahe <elahe.khabaz.bavani@gmail.com>
2023-02-24 21:59:01 +0330

    set cookie

--- uploadfile/upload.php
+++ uploadfile/upload.php
@@ -9,7 +9,7 @@
 <head>
 </head>
 <body>
-    <form action="/index.php" method="post" enctype="multipart/form-data">
+    <form action="/uploadfile/index.php" method="post" enctype="multipart/form-data">
         <input type="file" name="image">
         <button type="submit">upload</button>
     </form>
