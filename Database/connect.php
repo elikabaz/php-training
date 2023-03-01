c70917b5a525b6bb5a731960540e6d0665770554
elahe <elahe.khabaz.bavani@gmail.com>
2023-03-01 16:54:11 +0330

    m

--- Database/connect.php
+++ Database/connect.php
@@ -1,8 +1,9 @@
 <?php
+
 $connect=mysqli_connect('localhost:3306','root','');
 
 if(!$connect){
-    echo 'connection is:'. mysqli_connect_error();
+    echo 'connection :'. mysqli_connect_error();
     die();
 }
 echo 'connected';
